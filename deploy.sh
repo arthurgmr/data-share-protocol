9205a481f8ea30aae56e7156a4da11632972e6a5
Arthur Machado <arthur.machado@maxmilhas.com.br>
2024-03-22 23:03:33 -0300

    feat: deploy file

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+source .env
+
+forge script script/DataSphereProtocol.s.sol:DataSphereProtocolScript --rpc-url $RPC_URL --private-key $PV_KEY --broadcast
--- script/DataSphereProtocol.s.sol
+++ script/DataSphereProtocol.s.sol
@@ -10,7 +10,7 @@ contract DataSphereProtocolScript is Script {
     function run() public {
         DataSphereProtocol dataSphereProtocol;
 
-        vm.startBroadcast(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
+        vm.startBroadcast();
 
             dataSphereProtocol = new DataSphereProtocol();
 
